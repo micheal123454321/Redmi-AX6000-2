126d6193825ea6db8dfe1cd75fdc45732502ec07
Ne <3661428@gmail.com>
2025-05-22 14:15:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,8 +40,8 @@ mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
 # ##------------- meta core ---------------------------------
 curl -sL -m 30 --retry 2 https://github.com/MetaCubeX/mihomo/releases/download/v1.19.2/mihomo-linux-arm64-v1.19.2.gz -o /tmp/clash.gz
 gzip -d /tmp/clash.gz /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
+chmod +x /tmp/mihomo-linux-arm64 >/dev/null 2>&1
+mv /tmp/mihomo-linux-arm64 feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
 # ##---------------------------------------------------------
 
 # ##-------------- GeoIP 数据库 -----------------------------
